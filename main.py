73c97d73aad7b53a36fd1048410843ea6d0b881b
Felipe Aros <felipe.aros.r@gmail.com>
2024-04-04 23:23:12 -0300

    .

--- main.py
+++ main.py
@@ -20,4 +20,11 @@ def createImage():
       prompt=prompt, # from a yaml file
       model=model, # always dalle-3
       n=n, # depends on the type of user
+      quality=quality, # depends on the type of user
+      response_format="url",
+      size='1024x1792',
+      style="natural",
+      
     )
+
+    response = client.images.
